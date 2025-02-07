1017a1c2e050b16ede3af6666d6cf9e72c46abcb
Bia Brauna <167824411+biabrauna@users.noreply.github.com>
2025-02-07 10:34:46 -0300

    vite update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://api-register-users-ny35.onrender.com/'
+    }
+  },
   plugins: [react()],
 })
