8c66f4542eddf2a29e9b36364e8a254e83693c0a
biabrauna <biabn443@gmail.com>
2025-05-06 14:52:13 -0300

    feat: adding jwt parially

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Crie a instância do axios
 const api = axios.create({
   baseURL: 'http://localhost:3000/',
 });
@@ -8,40 +7,35 @@ const api = axios.create({
 // Interceptor para adicionar o token JWT em todas as requisições
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('access_token'); // Pegue o token do localStorage ou de onde você armazenou
+    const token = localStorage.getItem('access_token');
 
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`; // Adiciona o token no cabeçalho
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
 
-    return config; // Retorna a configuração da requisição
+    return config;
   },
   (error) => {
-    return Promise.reject(error); // Se houver erro na requisição, o erro é rejeitado
+    return Promise.reject(error);
   }
 );
 
 // Interceptor para lidar com os erros de resposta
 api.interceptors.response.use(
-  (response) => response, // Retorna a resposta normalmente
+  (response) => response,
   (error) => {
     if (error.response) {
-      // Handle specific error cases
       if (error.response.status === 401) {
-        // Handle unauthorized access
         console.error('Unauthorized access');
       } else if (error.response.status === 403) {
-        // Handle forbidden access
         console.error('Forbidden access');
       }
     } else if (error.request) {
-      // The request was made but no response was received
       console.error('No response received:', error.request);
     } else {
-      // Something happened in setting up the request
       console.error('Error setting up request:', error.message);
     }
-    return Promise.reject(error); // Retorna o erro para ser tratado onde o axios for chamado
+    return Promise.reject(error);
   }
 );
 
