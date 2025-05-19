6c1c7e8a0cfc0035bb615e0f819a860774cd9c63
biabrauna <biabn443@gmail.com>
2025-05-19 14:24:50 -0300

    feat: adding session for persistence

--- src/routes/SplashScreen.jsx
+++ src/routes/SplashScreen.jsx
@@ -11,12 +11,10 @@ export default function SplashScreen() {
     })
 
     return (
-        <BrowserRouter>
         <div style={{ backgroundImage: "url('https://res.cloudinary.com/dnulz0tix/image/upload/v1733910597/mc8vbxz0qmdqgr1e2bnn.png')", display: "flex", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
             <div className="App">
                 <img src="https://res.cloudinary.com/dnulz0tix/image/upload/v1733910597/jhijfhatit6mcroyiqy3.png" className="App-logo" alt="logo" />
             </div>
         </div>
-        </BrowserRouter>
     );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import history from 'connect-history-api-fallback';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,
     proxy: {
       '/auth': {
         target: 'https://airy-charm-production.up.railway.app/',
         changeOrigin: true,
         secure: false,
       }
-    }
-  }
-})
+    },
+    middlewareMode: false, // necessário para setupMiddlewares funcionar
+    setupMiddlewares: (middlewares, devServer) => {
+      middlewares.use(
+        history({
+          verbose: true,
+          disableDotRule: true, // importante para permitir arquivos com ponto (ex: .js, .css)
+        })
+      );
+      return middlewares;
+    },
+  },
+});
