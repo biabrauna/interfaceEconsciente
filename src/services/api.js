ccbb8513ef56319afdaca1b77ec6aced5cffcb85
biabrauna <biabn443@gmail.com>
2025-06-10 21:42:44 -0300

    feat: changing backeend to localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://airy-charm-production.up.railway.app/',
+    baseURL: 'http://localhost:3000',
     withCredentials: true,
 })
 
